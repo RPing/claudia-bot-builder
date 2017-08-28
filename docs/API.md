1d909987f3088ff39a06a98cfbc8b4e81900b5ee
rpchen <g1222888@gmail.com>
2017-08-28 20:28:24 +0800

    Add subtype property in Slack parse

--- docs/API.md
+++ docs/API.md
@@ -37,6 +37,7 @@ The message object contains the following fields
 * **`originalRequest`**: `object` the complete original message, in a bot-specific format, useful if you want to do more than just reply to text messages.
 * **`sender`**: `string` the identifier of the sender
 * **`postback`**: `boolean` true if the message is the result of a post-back (for example clicking on a button created by a previous message in Facebook). It will be `undefined` (falsy) for completely new messages.
+* **`subtype`**: `string` only appear on Slack platform. It can be `slack-slash-command`, `slack-message-action` or `slack-events-api`.
 
 _Note_: FB Messenger message echoes, delivery and read reports will not be parsed.
 
--- spec/slack/slack-parse-spec.js
+++ spec/slack/slack-parse-spec.js
@@ -11,22 +11,28 @@ describe('Slack parse', () => {
     expect(parse([1, 2, 3])).toBeUndefined();
   });
 
-  it('returns nothing if user_id and actions are missing', () => {
+  it('returns nothing if user_id, actions and type are missing, or type is not event_callback', () => {
     expect(parse({text: 'pete'})).toBeUndefined();
+    expect(parse({ type: 'XXX', event: { user: 123, text: 'hello' } })).toBeUndefined();
   });
 
   it('returns an empty text if the text is missing', () => {
     var msg = { user_id: 123 };
-    expect(parse(msg)).toEqual({ sender: 123, text: '', originalRequest: msg, type: 'slack-slash-command'});
+    expect(parse(msg)).toEqual({ sender: 123, text: '', originalRequest: msg, type: 'slack-slash-command', subtype: 'slack-slash-command'});
   });
 
   it('returns a parsed object when text and user_id are present', () => {
     var msg = { user_id: 123, text: 'Hello' };
-    expect(parse(msg)).toEqual({ sender: 123, text: 'Hello', originalRequest: msg, type: 'slack-slash-command'});
+    expect(parse(msg)).toEqual({ sender: 123, text: 'Hello', originalRequest: msg, type: 'slack-slash-command', subtype: 'slack-slash-command'});
   });
 
   it('returns a parsed object when actions are present', () => {
     var msg = { user: { id: 123, name: 'username' }, actions: [{name: 'test', value: 'action'}] };
-    expect(parse(msg)).toEqual({ sender: 123, text: '', originalRequest: msg, type: 'slack-message-action', postback: true});
+    expect(parse(msg)).toEqual({ sender: 123, text: '', originalRequest: msg, type: 'slack-slash-command', subtype: 'slack-message-action', postback: true});
+  });
+
+  it('returns a parsed object when type is event_callback', () => {
+    var msg = { type: 'event_callback', event: { user: 123 } };
+    expect(parse(msg)).toEqual({ sender: 123, text: '', originalRequest: msg, type: 'slack-slash-command', subtype: 'slack-events-api'});
   });
 });
